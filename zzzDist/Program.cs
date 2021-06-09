330637ec0ed942577db9b65c71f83e1c1bb1495e
ambiesoft <ambiesoft.trueff@gmail.com>
2021-06-09 12:47:20 +0900

    dist

---
+++ zzzDist/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zzzDist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
